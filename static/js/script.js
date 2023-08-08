d252ff092697f340bb97d6f3d2364216d20914e7
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-08 17:05:54 +0300

    methods for finding contact i AddressBook function

--- static/js/script.js
+++ static/js/script.js
@@ -20,3 +20,11 @@ function AddressBook() {
     this.currentId += 1;
     return this.currentId;
   };
+  
+  AddressBook.prototype.findContact = function (id) {
+    if (this.contacts[id] !== undefined) {
+      return this.contacts[id];
+    }
+    return false;
+  };
+  

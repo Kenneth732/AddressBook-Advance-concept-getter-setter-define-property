bde113611e11d29cef4f2c6d918455643a1c1585
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-08 17:03:38 +0300

    addressbook constructor function but i am using onject property to define my contact and id for each contact saved in object

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+// Business Logic for AddressBook ---------
+function AddressBook() {
+    Object.defineProperty(this, "contacts", {
+      value: {},
+      writable: true,
+    });
+  
+    Object.defineProperty(this, "currentId", {
+      value: 0,
+      writable: true,
+    });
+  }
+  
